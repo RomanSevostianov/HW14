7f5386fa1ecc53c2509f464adcf74b5f4644913a
Роман <pangujailbreake58@gmail.com>
2022-11-23 17:05:08 +0300

    One

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Human Maksim = new Human("Максим", "Москва",1985,"бренд меннеджер");
+        Human Anna = new Human("Анна", "Саратов",1975,"методист образовательных программ");
+        Human Katy = new Human("Катя ", "Пенза",1995,"прадакт менеджер");
+        Human Artem = new Human("Артем ", "Калининград",1990,"директор по развитию бизнеса");
+        Maksim.enter();
+        Anna.enter();
+        Katy.enter();
+        Artem.enter();
+
+
+        Cars Lada = new Cars("Lada ", " Granta ",1.9," желтый  ",2003," Россия");
+        Cars VW = new Cars("VW ", " Tiguan ",2.0," желтый  ",2023," Германия");
+
+        Cars Audi = new Cars("Audi ", " Q3 ",2.4," черный ",2020," Германия");
+        Lada.enterCars();
+        Audi.enterCars();
+        VW.enterCars();
     }
 }
